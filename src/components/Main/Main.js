cddd39a3656ba0175b2068b8109ea78894699912
Austin Peel <austinpeel@gmail.com>
2022-02-08 15:48:24 +0100

    Update text descriptions. Add chapter 1 gif

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,7 @@
 import './Main.css';
 import Chapter from '../Chapter/Chapter';
-import image1 from './Chapter1.png';
+import gif1 from './Chapter1.gif';
+// import image1 from './Chapter1.png';
 import image2 from './Chapter2.png';
 import image3 from './Chapter3.png';
 
@@ -8,14 +9,15 @@ const chapter1 = {
   index: 1,
   title: 'Planetary Motion',
   description:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-  image: image1,
+    "Explore the motion of celestial bodies through interactive apps. Understand Kepler's famous three laws of planetary motion.",
+  image: gif1,
   apps: [
     'Two-Body Problem',
     "Kepler's First Law",
     "Kepler's Second Law",
     "Kepler's Third Law",
   ],
+  status: ['Complete', 'Complete', 'Complete', 'Complete'],
   link: 'https://epfl-astrophysics-i.github.io/chapter1/',
 };
 
@@ -23,9 +25,10 @@ const chapter2 = {
   index: 2,
   title: 'Tidal Forces',
   description:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    "Visualize tidal forces on Earth arising from the Moon's gravitational pull. Explore the physical mechanism behind the formation of Saturn's rings.",
   image: image2,
   apps: ['Tidal Forces', 'Roche Limit', 'Tidal Locking'],
+  status: ['Complete', 'InProgress', 'InProgress'],
   link: 'https://epfl-astrophysics-i.github.io/chapter2/',
 };
 
@@ -33,9 +36,10 @@ const chapter3 = {
   index: 3,
   title: 'Virial Theorem',
   description:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    'Discover how kinetic and potential energy are related in systems containing many gravitating bodies. See how we can weigh massive and distant celestial objects.',
   image: image3,
   apps: ['Virial Theorem', 'Mass of a Globular Cluster', 'N-Body Accuracy'],
+  status: ['InProgress', 'InProgress', 'InProgress'],
   link: 'https://epfl-astrophysics-i.github.io/chapter3/',
 };
 
