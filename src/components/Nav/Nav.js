457c28de77bb96d9096025adb6c254165f6b054a
Austin Peel <austinpeel@gmail.com>
2022-05-13 14:02:30 +0200

    Add github link to navbar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,8 +1,13 @@
 import './Nav.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 export default function Nav() {
   return (
     <nav className='nav'>
+      <a href='https://github.com/EPFL-Astrophysics-I'>
+        <FontAwesomeIcon icon={faGithub} />
+      </a>
       <a href='https://epfl-astrophysics-i.github.io/home/'>Home</a>
       <a href='https://epfl-astrophysics-i.github.io/chapter1/'>Chapter 1</a>
       <a href='https://epfl-astrophysics-i.github.io/chapter2/'>Chapter 2</a>
