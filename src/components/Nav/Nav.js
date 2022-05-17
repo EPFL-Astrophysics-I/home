0a9932ac4fb1b973920b8850ea3f8b7cd3c240bb
Austin Peel <austinpeel@gmail.com>
2022-05-17 14:52:55 +0200

    Increase github icon size

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,7 +6,7 @@ export default function Nav() {
   return (
     <nav className='nav'>
       <a href='https://github.com/EPFL-Astrophysics-I'>
-        <FontAwesomeIcon icon={faGithub} />
+        <FontAwesomeIcon icon={faGithub} size='lg' />
       </a>
       <a href='https://epfl-astrophysics-i.github.io/home/'>Home</a>
       <a href='https://epfl-astrophysics-i.github.io/chapter1/'>Chapter 1</a>
